90ebed8ac4142f305e63d3ee70065b960ec5480c
Abdelrahman Eed <18529757+SharkMan201@users.noreply.github.com>
2025-05-31 14:49:29 +0200

    Add device selection

--- hello_triangle/main.cc
+++ hello_triangle/main.cc
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
 #include <vector>
 
 namespace HelloTriangle {
@@ -102,6 +103,58 @@ private:
   void initVulkan() {
     createInstance();
     setupDebugMessenger();
+    pickPhysicalDevice();
+  }
+
+  void pickPhysicalDevice() {
+    uint32_t device_count = 0;
+    vkEnumeratePhysicalDevices(instance_, &device_count, nullptr);
+
+    if (device_count == 0) {
+      throw std::runtime_error("failed to find GPUs with Vulkan support!");
+    }
+
+    std::vector<VkPhysicalDevice> devices(device_count);
+    vkEnumeratePhysicalDevices(instance_, &device_count, devices.data());
+
+    // Use an ordered map to automatically sort candidates by increasing score
+    std::multimap<int, VkPhysicalDevice> candidates;
+
+    for (const auto &device : devices) {
+      int score = rateDeviceSuitability(device);
+      candidates.insert(std::make_pair(score, device));
+    }
+
+    // Check if the best candidate is suitable at all
+    if (candidates.rbegin()->first > 0) {
+      physical_device_ = candidates.rbegin()->second;
+    } else {
+      throw std::runtime_error("failed to find a suitable GPU!");
+    }
+  }
+
+  int rateDeviceSuitability(VkPhysicalDevice device) {
+    VkPhysicalDeviceProperties device_properties;
+    VkPhysicalDeviceFeatures device_features;
+    vkGetPhysicalDeviceProperties(device, &device_properties);
+    vkGetPhysicalDeviceFeatures(device, &device_features);
+
+    int score = 0;
+
+    // Discrete GPUs have a significant performance advantage
+    if (device_properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+      score += 1000;
+    }
+
+    // Maximum possible size of textures affects graphics quality
+    score += device_properties.limits.maxImageDimension2D;
+
+    // Application can't function without geometry shaders
+    if (!device_features.geometryShader) {
+      return 0;
+    }
+
+    return score;
   }
 
   void createInstance() {
@@ -214,9 +267,10 @@ private:
     glfwTerminate();
   }
 
-  GLFWwindow *window_;
-  VkInstance instance_;
-  VkDebugUtilsMessengerEXT debug_messenger_;
+  GLFWwindow *window_ = nullptr;
+  VkInstance instance_ = nullptr;
+  VkDebugUtilsMessengerEXT debug_messenger_ = nullptr;
+  VkPhysicalDevice physical_device_ = VK_NULL_HANDLE;
 };
 } // namespace HelloTriangle
 
