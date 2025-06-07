ed76a5e977affed0c056c60209bbcba3a17975b6
Abdelrahman Eed <18529757+SharkMan201@users.noreply.github.com>
2025-06-07 18:27:10 +0200

    pass in color from the vertex shader

--- learn_opengl/main.cc
+++ learn_opengl/main.cc
@@ -23,17 +23,19 @@ void processInput(GLFWwindow *window) {
 const char *vertex_shader_source =
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);"
     "}\0";
 
-const char *fragment_shader_source =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main() {\n"
-    "  FragColor =  vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0";
+const char *fragment_shader_source = "#version 330 core\n"
+                                     "in vec4 vertexColor;"
+                                     "out vec4 FragColor;\n"
+                                     "void main() {\n"
+                                     "  FragColor =  vertexColor;\n"
+                                     "}\0";
 
 int main() {
   glfwInit();
